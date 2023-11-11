11aeb07dea548ae4b0bb6bb2a84ae202bc9b0a27
Giani Statie <gianistatie@gmail.com>
2023-11-11 21:52:29 +0200

    Fixed UI bugs

--- InputTransform.gd
+++ InputTransform.gd
@@ -3,7 +3,7 @@ extends HBoxContainer
 signal transformChanged(_transform)
 @export var transformName:String = "undefined"
 
-var transform = Vector3()
+var transform = Vector3.ZERO : set = _set_transform, get = _get_transform
 @onready var transformX = $LabelScale/ScaleX
 @onready var transformY = $LabelScale/ScaleY
 @onready var transformZ = $LabelScale/ScaleZ
@@ -23,3 +23,12 @@ func _on_scale_y_text_changed():
 func _on_scale_z_text_changed():
 	transform.z = float(transformZ.text)
 	emit_signal("transformChanged",transform)
+
+func _set_transform(value):
+	transform = value
+	transformX.text = str(value.x)
+	transformY.text = str(value.y)
+	transformZ.text = str(value.z)
+
+func _get_transform():
+	return transform
--- main.gd
+++ main.gd
@@ -3,33 +3,40 @@ extends Control
 @export var player_canvas_path: NodePath
 @export var file_button_path: NodePath
 
+@export var position_ui_path: NodePath
+@export var rotation_ui_path: NodePath
+@export var scale_ui_path: NodePath
+
 var player_canvas: SubViewportContainer
 var file_button: MenuButton
 
+var position_ui: HBoxContainer
+var rotation_ui: HBoxContainer
+var scale_ui: HBoxContainer
+
 var player_node: Node3D
 var color_shader: ColorRect
-
 var player_transform: Node3D
-var player_position: Node3D
-
-
-var scaleVector = Vector3()
 
 
 func _ready():
 	player_canvas = get_node(player_canvas_path)
 	file_button = get_node(file_button_path)
+	position_ui = get_node(position_ui_path)
+	rotation_ui = get_node(rotation_ui_path)
+	scale_ui = get_node(scale_ui_path)
 	
 	color_shader = player_canvas.get_node("Viewport/ColorShader")
 	player_node = player_canvas.get_node("Viewport/Player")
 	file_button.connect("model_load_triggered", _on_model_load_triggered)
 	
 	player_transform = player_canvas.get_node("Viewport/Player")
-	player_position = player_canvas.get_node("Viewport/Player")
-
+	
 	var c = Callable(self,"file_drop_path")
 	get_tree().get_root().connect('files_dropped',c)
 	
+	update_player_transform()
+
 
 func _on_background_shader_toggled(button_pressed):
 	if not button_pressed:
@@ -37,10 +44,12 @@ func _on_background_shader_toggled(button_pressed):
 	else:
 		color_shader.hide()
 
+
 func _on_run_annimation_button_down():
 	var img: Image
 	img = await player_canvas.get_all_animation_frames()
 	img.save_png('test.png')
+
 
 func _on_model_load_triggered(path):
 	var state = GLTFState.new()
@@ -58,9 +67,16 @@ func _on_model_load_triggered(path):
 	player_node = node
 	player_node.name = "Player"
 
+	update_player_transform()
+
+
+func update_player_transform():
+	player_transform = player_canvas.get_node("Viewport/Player")
+	position_ui.transform = player_transform.position
+	rotation_ui.transform = player_transform.rotation_degrees
+	scale_ui.transform = player_transform.scale
 
 func file_drop_path(files):
-	print(files[0])
 	_on_model_load_triggered(files[0])
 
 
@@ -69,7 +85,6 @@ func _on_position_transform_changed(_transform):
 
 
 func _on_rotation_transform_changed(_transform):
-	
 	player_transform.rotation_degrees=_transform
 
 
