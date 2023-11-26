f2b699fededac36c49e2531cfb06125c1843cf9a
Ajgamerteam <90553545+Ajgamerteam@users.noreply.github.com>
2023-11-26 17:12:26 +0300

    Update MenuButton.gd

--- MenuButton.gd
+++ MenuButton.gd
@@ -13,10 +13,12 @@ func file_menu(id):
 			file_dialog.file_mode = 0
 			file_dialog.add_filter('glb')
 			file_dialog.add_filter('gltf')
+			file_dialog.ok_button_text = 'Select 3D model'
 			file_dialog.show()
 		1:
 			file_dialog.file_mode = 2
 			file_dialog.clear_filters()
+			file_dialog.ok_button_text = 'Select directory to save sprite sheets'
 			file_dialog.show()
 			print("open")
 		2:
