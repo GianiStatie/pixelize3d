95cc8e9416a79fb6b9ac8efc8d605a3637362184
Ajgamerteam <90553545+Ajgamerteam@users.noreply.github.com>
2023-11-26 16:53:10 +0300

    Update MenuButton.gd

--- MenuButton.gd
+++ MenuButton.gd
@@ -10,13 +10,14 @@ func _ready():
 func file_menu(id):
 	match id:
 		0:
-			#open
 			file_dialog.file_mode = 0
+			file_dialog.add_filter('glb')
+			file_dialog.add_filter('gltf')
 			file_dialog.show()
 		1:
 			file_dialog.file_mode = 2
+			file_dialog.clear_filters()
 			file_dialog.show()
-			#load_model_file_dialog.
 			print("open")
 		2:
 			print("save")
@@ -26,8 +27,6 @@ func file_menu(id):
 
 func get_dir():
 	file_dialog.show()
-	
-	pass
 
 func _on_load_model_file_dialog_file_selected(path):
 	emit_signal("model_load_triggered", path)
