b18b19ebbfeba9a11baa3539059b2de47b954bf8
Giani Statie <gianistatie@gmail.com>
2023-11-27 09:09:36 +0200

    Fixing input dialogs

--- InputTransform.gd
+++ InputTransform.gd
@@ -12,23 +12,26 @@ var transform = Vector3.ZERO : set = _set_transform, get = _get_transform
 func _ready():
 	labelName.text = transformName
 
-func _on_scale_x_text_changed(new):
-	transform.x = float(new)
-	emit_signal("transformChanged",transform)
-
-func _on_scale_y_text_changed(new):
-	transform.y = float(new)
-	emit_signal("transformChanged",transform)
-
-func _on_scale_z_text_changed(new):
-	transform.z = float(new)
-	emit_signal("transformChanged",transform)
-
 func _set_transform(value):
 	transform = value
-	#transformX.text = str(value.x)
-	#transformY.text = str(value.y)
-	#transformZ.text = str(value.z)
+	transformX.text = str(value.x)
+	transformY.text = str(value.y)
+	transformZ.text = str(value.z)
 
 func _get_transform():
 	return transform
+
+func _on_scale_x_text_submitted(new_text):
+	transform.x = float(new_text)
+	transformX.text = new_text
+	emit_signal("transformChanged",transform)
+
+func _on_scale_y_text_submitted(new_text):
+	transform.y = float(new_text)
+	transformY.text = new_text
+	emit_signal("transformChanged",transform)
+
+func _on_scale_z_text_submitted(new_text):
+	transform.z = float(new_text)
+	transformZ.text = new_text
+	emit_signal("transformChanged",transform)
